f0c6f1671046a6d06494bc628869bd4e495514a4
revert <revertcreations@gmail.com>
2021-06-16 16:17:04 -0600

    small email verbage change

--- resources/views/admin/emails/photography/contract.blade.php
+++ resources/views/admin/emails/photography/contract.blade.php
@@ -3,7 +3,7 @@
 
 Hey {{ $contract->client->first_name }} {{ $contract->client->last_name }},
 
-Below is a link to our contract for the upcoming <b>{{ $contract->photoshoot->title }}</b>. Nothing is final until you check the box at the bottom, and click the "I
+Below is a link to our contract proposal for the upcoming <b>{{ $contract->photoshoot->title }}</b>. Nothing is final until you check the box at the bottom, and click the "I
 Agree" button. You are able to edit most of the details. If there are any changes I'll recieve a notification, look over the edits,
 and get back to you asap.<br>
 
@@ -11,7 +11,7 @@ Feel free to email me back with any questions or concerns.
 
 
 @component('mail::button', ['url' => route('public.photoshoot.edit', ['photoshoot' => $contract->photoshoot->id, 'token' => base64_encode($contract->photoshoot->public_token)])])
-View Contract Draft
+Contract Proposal
 @endcomponent
 
 Look forward to working with you,<br><br>
