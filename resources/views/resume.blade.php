850aea4fc933c19213ae153a7f58ef7062179213
revertcreations <revertcreations@gmail.com>
2026-03-16 12:37:58 -0400

    remove canvas width and height for resume

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -37,7 +37,7 @@
                 const canvas = document.createElement('canvas');
                 canvas.width = renderViewport.width;
                 canvas.height = renderViewport.height;
-                canvas.style.cssText = `position:absolute;top:0;left:0;width:${displayViewport.width}px;height:${displayViewport.height}px;`;
+                canvas.style.cssText = `position:absolute;top:0;left:0;`;
 
                 pageWrapper.appendChild(canvas);
                 container.appendChild(pageWrapper);
